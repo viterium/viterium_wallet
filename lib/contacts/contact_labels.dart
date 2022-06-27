3968887e1a676735c754b3610877a876ea5111ff
Iulian Blajut <iulian.blajut@gmail.com>
2022-06-27 10:41:41 +0300

    Refactor contact labels

---
+++ lib/contacts/contact_labels.dart
@@ -0,0 +1,62 @@
+import 'contact.dart';
+
+final labeledAddresses = Map.fromEntries(
+  const [
+    Contact(
+      name: '#Quota',
+      address: 'vite_0000000000000000000000000000000000000003f6af7459b9',
+    ),
+    Contact(
+      name: '#Consensus',
+      address: 'vite_0000000000000000000000000000000000000004d28108e76b',
+    ),
+    Contact(
+      name: '#TokenIssuance',
+      address: 'vite_000000000000000000000000000000000000000595292d996d',
+    ),
+    Contact(
+      name: '#VitexFund',
+      address: 'vite_0000000000000000000000000000000000000006e82b8ba657',
+    ),
+    Contact(
+      name: '#VitexTrade',
+      address: 'vite_00000000000000000000000000000000000000079710f19dc7',
+    ),
+    Contact(
+      name: '#VivaStaking v0.1',
+      address: 'vite_65ea4fbb8fc4a0f5cac745e0a97844ff2e9e4287aa0c35a28f',
+    ),
+    Contact(
+      name: '#VivaStaking v0.2',
+      address: 'vite_6b97e21d57909a881db29385b2339725ebbdf708b9eb487985',
+    ),
+    Contact(
+      name: '#VivaStaking v0.3',
+      address: 'vite_6217fbda565d83ea03d23034054fb036a0d7a540993354e1e6',
+    ),
+    Contact(
+      name: '#VivaStaking v0.4',
+      address: 'vite_12e90482e9e13bbbeb1919e7993ffadd375f6b7aa483452cbf',
+    ),
+    Contact(
+      name: '#VITCStake v0.1',
+      address: 'vite_cc2d0c2c34ae6af8bd58e111ca8c958d002c2b3199f449c8d7',
+    ),
+    Contact(
+      name: '#VITCStake v0.2',
+      address: 'vite_c85b335e221fc99631785f3d579edd1b7a2691131b7f3998a3',
+    ),
+    Contact(
+      name: '#VITCSwap',
+      address: 'vite_29ae0b9f951323b3bfe9bb8251bba2830eddacf51631630495',
+    ),
+    Contact(
+      name: '#VitaBot',
+      address: 'vite_178bc3256ac2b30cc923cd0c5f138e79b8b7257e43f69606f3',
+    ),
+    Contact(
+      name: '#VitePunks',
+      address: 'vite_fd06f4ef9a32fcabcdcae1857c1cab18b496ef1eafd1b4edc1',
+    ),
+  ].map((e) => MapEntry(e.address, e)),
+);
--- lib/contacts/contacts_notifier.dart
+++ lib/contacts/contacts_notifier.dart
@@ -3,60 +3,10 @@ import 'package:viterium_wallet/util/sharedprefsutil.dart';
 
 import '../database/boxes.dart';
 import 'contact.dart';
+import 'contact_labels.dart';
 
 const defaultContacts = {
   'vite_0608e2a5caa926d664d6c7f9992befae6b9dca5347d1858e4e': '@ViteriumTeam',
-};
-
-const labeledAddresses = {
-  'vite_0000000000000000000000000000000000000003f6af7459b9': const Contact(
-    name: '#Quota',
-    address: 'vite_0000000000000000000000000000000000000003f6af7459b9',
-  ),
-  'vite_0000000000000000000000000000000000000004d28108e76b': const Contact(
-    name: '#Consensus',
-    address: 'vite_0000000000000000000000000000000000000004d28108e76b',
-  ),
-  'vite_000000000000000000000000000000000000000595292d996d': const Contact(
-    name: '#TokenIssuance',
-    address: 'vite_000000000000000000000000000000000000000595292d996d',
-  ),
-  'vite_0000000000000000000000000000000000000006e82b8ba657': const Contact(
-    name: '#VitexFund',
-    address: 'vite_0000000000000000000000000000000000000006e82b8ba657',
-  ),
-  'vite_00000000000000000000000000000000000000079710f19dc7': const Contact(
-    name: '#VitexTrade',
-    address: 'vite_00000000000000000000000000000000000000079710f19dc7',
-  ),
-  'vite_65ea4fbb8fc4a0f5cac745e0a97844ff2e9e4287aa0c35a28f': const Contact(
-    name: '#VivaStaking v0.1',
-    address: 'vite_65ea4fbb8fc4a0f5cac745e0a97844ff2e9e4287aa0c35a28f',
-  ),
-  'vite_6b97e21d57909a881db29385b2339725ebbdf708b9eb487985': const Contact(
-    name: '#VivaStaking v0.2',
-    address: 'vite_6b97e21d57909a881db29385b2339725ebbdf708b9eb487985',
-  ),
-  'vite_6217fbda565d83ea03d23034054fb036a0d7a540993354e1e6': const Contact(
-    name: '#VivaStaking v0.3',
-    address: 'vite_6217fbda565d83ea03d23034054fb036a0d7a540993354e1e6',
-  ),
-  'vite_12e90482e9e13bbbeb1919e7993ffadd375f6b7aa483452cbf': const Contact(
-    name: '#VivaStaking v0.4',
-    address: 'vite_12e90482e9e13bbbeb1919e7993ffadd375f6b7aa483452cbf',
-  ),
-  'vite_cc2d0c2c34ae6af8bd58e111ca8c958d002c2b3199f449c8d7': const Contact(
-    name: '#BeefStake v0.1',
-    address: 'vite_cc2d0c2c34ae6af8bd58e111ca8c958d002c2b3199f449c8d7',
-  ),
-  'vite_c85b335e221fc99631785f3d579edd1b7a2691131b7f3998a3': const Contact(
-    name: '#BeefStake v0.2',
-    address: 'vite_c85b335e221fc99631785f3d579edd1b7a2691131b7f3998a3',
-  ),
-  'vite_178bc3256ac2b30cc923cd0c5f138e79b8b7257e43f69606f3': const Contact(
-    name: '#VitaBot',
-    address: 'vite_178bc3256ac2b30cc923cd0c5f138e79b8b7257e43f69606f3',
-  ),
 };
 
 class ContactsNotifier extends ChangeNotifier {
