b6fd3b2091d84b4ebb2cd6187a0b1ec230ce1802
Azbuky <azbuky@gmail.com>
2023-03-15 17:45:10 +0200

    Update asset tokens

--- lib/tokens/token_constants.dart
+++ lib/tokens/token_constants.dart
@@ -1,14 +1,18 @@
 const defaultTokenIconPath = 'assets/token_icons/tti_default.png';
 
 const testnetTokenIdMapping = {
-  'tti_2511d9c93e4f98318eef6f56': 'tti_a23c2f75791efafe5fada99e',
-  'tti_753314114cfca5082da60a8a': 'tti_5070454b58acf37293c7a0e9',
-  'tti_fe5446bd65e41854d8c3f6da': 'tti_5070454b58acf37293c7a0e9',
+  'tti_cdbf9d8cfa63feaeed13c2f0': 'tti_b18e6488eeb30541da7f5010', // VT
+  'tti_9a20ef2ed026aa2d13b178d6': 'tti_22d0b205bed4d268a05dfc3c', // VITC
+  'tti_9bea41f4ddd347a60335c6b2': 'tti_541b25bd5e5db35166864096', // VINU
+  'tti_973afc9ffd18c4679de42e93': 'tti_80f3751485e4e83456059473', // USDT
+  'tti_322862b3f8edae3b02b110b1': 'tti_b90c9baffffc9dae58d1f33f', // BTC
+  'tti_2be836f7e90b34c061de9f7b': 'tti_fb901364ef662c25cf43125c', // BNB
+  'tti_06822f8d096ecdf9356b666c': 'tti_06822f8d096ecdf9356b666c', // ETH
 };
 
 const assetTokenIds = {
-  'tti_251a3e67a41b5ea2373936c8',
-  'tti_5070454b58acf37293c7a0e9',
+  'tti_b18e6488eeb30541da7f5010',
   'tti_5649544520544f4b454e6e40',
   'tti_564954455820434f494e69b5',
+  'tti_251a3e67a41b5ea2373936c8',
 };
