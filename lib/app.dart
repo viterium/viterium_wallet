84cc7eef377ed2d7db07bf11f936d9f8fa6a9c34
Azbuky <azbuky@gmail.com>
2024-04-23 11:43:09 +0300

    Disable material 3

--- lib/app.dart
+++ lib/app.dart
@@ -78,6 +78,7 @@ class App extends HookConsumerWidget {
                 debugShowCheckedModeBanner: false,
                 title: 'Viterium',
                 theme: ThemeData(
+                  useMaterial3: false,
                   dialogBackgroundColor: theme.backgroundDark,
                   primaryColor: theme.primary,
                   colorScheme: ThemeData().colorScheme.copyWith(
